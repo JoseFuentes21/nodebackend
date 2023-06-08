c400508a47dee5066acc7e7f0a33b2fac2c266d1
Jose Fuentes <fo17007@ues.edu.sv>
2023-06-08 15:40:19 -0600

    carpetas

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
 require('dotenv').config()
 const http = require("http")
+const fs = require("fs")
 
-function requestController() {
+function requestController(req, res) {
   // Lógica de nuestra función
-  console.log("Hola mundo!!")
+  const url = req.url;
+  const method = req.method;
+  console.log({url, method})
+
+   if(method === 'GET' && url === "/"){
+        res.setHeader("Content-type", "text/html; charset=utf-8")
+        fs.readFile('./public/index.html', function(error, file){
+            if(error){
+                console.log("HUBO ERROR")
+            }
+            res.write(file)
+            res.end()
+        })
+        return
+    }
+
+    if(method === 'GET' && url === "/about"){
+        res.setHeader("Content-type", "text/html; charset=utf-8")
+        res.write("<h1>Hola mundo desde pagina ABOUT</h1>")
+        res.end()
+        return
+    }
+    res.setHeader("Content-type", "text/html; charset=utf-8")
+        res.write("<h1>Pagina no encontrada</h1>")
+        res.end()
+
 }
 
 // Configurar nuestro servidor
